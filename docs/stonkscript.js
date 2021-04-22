7a90997bd76d0cd3db9a14f66875d3251ce6f4fd
anatems1 <mrs196@pitt.edu>
2021-04-22 16:27:27 -0400

    added input ticker form and submit

--- docs/stonkscript.js
+++ docs/stonkscript.js
@@ -8,9 +8,9 @@ function init() {
 /*GLOBAL VARIABLES********%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 stock = ['GME', 'DOGE', 'AAPL', 'TSLA', 'AMD', 'NIO', 'MSFT', 'ENPH', 'BTC', 'SONY'];
 
-let pcnt = new Array (10);
+let pcnt = new Array(10);
 
-for (let i=0; i<10; i++){
+for (let i = 0; i < 10; i++) {
   pcnt[i] = (Math.random() < 0.5 ? -1 : 1) * (Math.floor(Math.random() * 1000) + 1)
 }
 
@@ -19,9 +19,9 @@ for (let i=0; i<10; i++){
 function topmovers() {
   for (var i = 0; i < 10; i++) {
     document.getElementById("floppy" + (i + 1)).textContent = stock[i];
-    if (pcnt[i]<0){
+    if (pcnt[i] < 0) {
       document.getElementById("floppy" + (i + 1)).style.color = "#cc5959";
-    }else {
+    } else {
       document.getElementById("floppy" + (i + 1)).style.color = "#1dd623";
     }
   }
@@ -31,84 +31,97 @@ function topmovers() {
 function stockshow(choice) {
   /*fades static on screen away*/
   var fadeTarget = document.getElementById('screen');
-    document.getElementById('ticker').textContent = "";
+  document.getElementById('ticker').textContent = "";
 
   /*document.getElementById('monitor').style.display = "none";*/
-  if (pcnt[choice]<0){
+  if (pcnt[choice] < 0) {
     document.getElementById('ticker').style.color = "#cc5959";
     strkcol = "#cc5959";
-      document.getElementById('ticker').textContent = stock[choice] + " "+ pcnt[choice] +"%";
-  }else {
+    document.getElementById('ticker').textContent = stock[choice] + " " + pcnt[choice] + "%";
+  } else if (pcnt[choice] > 0) {
     document.getElementById('ticker').style.color = "#1dd623";
     strkcol = "#1dd623";
-      document.getElementById('ticker').textContent = stock[choice] + " +"+ pcnt[choice] +"%";
-  }
+    document.getElementById('ticker').textContent = stock[choice] + " +" + pcnt[choice] + "%";
+  } else {
+    pcnt1 = (Math.random() < 0.5 ? -1 : 1) * (Math.floor(Math.random() * 1000) + 1);
+    stock1 = document.getElementById('tickertxt').value;
 
-
-
-  var fadeEffect = setInterval(function() {
-    if (!fadeTarget.style.opacity) {
-      fadeTarget.style.opacity = 1;
+    if (pcnt1 < 0) {
+      document.getElementById('ticker').style.color = "#cc5959";
+      strkcol = "#cc5959";
+      document.getElementById('ticker').textContent = stock1 + " " + pcnt1 + "%";
+    } else if (pcnt1 > 0) {
+      document.getElementById('ticker').style.color = "#1dd623";
+      strkcol = "#1dd623";
+      document.getElementById('ticker').textContent = stock1 + " +" + pcnt1 + "%";
     }
-    if (fadeTarget.style.opacity > 0) {
-      fadeTarget.style.opacity -= 0.1;
-    } else {
-      clearInterval(fadeEffect);
-    }
-  }, 100);
-
-  var cnv = document.getElementById('chartarea');
-  var ctx = cnv.getContext('2d');
-
-
-  var scale = 50;
-  var xlimit = (cnv.width) / scale;
-  var ylimit = (cnv.height) / scale;
-  var xwidth = cnv.width;
-  var yheight = cnv.height;
-
-  ctx.clearRect(0, 0, xwidth, yheight);
-
-  var countx = 0;
-  var county = 0;
-
-  for (var i = 1; i < xwidth - xlimit; i++) {
-    j = i * xlimit;
-    ctx.beginPath();
-    ctx.lineWidth=0.5;
-    ctx.strokeStyle = '#1dd623';
-    ctx.moveTo(j, ylimit);
-    ctx.lineTo(j, yheight);
-    ctx.stroke(); // Draw it
-    ctx.closePath();
-    countx+=1;
-  }
-  for (var i = 1; i < yheight - ylimit; i++) {
-    ctx.beginPath();
-    ctx.lineWidth=0.5;
-    ctx.strokeStyle = '#1dd623';
-    j = i * ylimit;
-    ctx.moveTo(xlimit, j);
-    ctx.lineTo(xwidth, j);
-    ctx.stroke(); // Draw it
-    ctx.closePath();
-
   }
 
+
+
+var fadeEffect = setInterval(function() {
+  if (!fadeTarget.style.opacity) {
+    fadeTarget.style.opacity = 1;
+  }
+  if (fadeTarget.style.opacity > 0) {
+    fadeTarget.style.opacity -= 0.1;
+  } else {
+    clearInterval(fadeEffect);
+  }
+}, 100);
+
+var cnv = document.getElementById('chartarea');
+var ctx = cnv.getContext('2d');
+
+
+var scale = 50;
+var xlimit = (cnv.width) / scale;
+var ylimit = (cnv.height) / scale;
+var xwidth = cnv.width;
+var yheight = cnv.height;
+
+ctx.clearRect(0, 0, xwidth, yheight);
+
+var countx = 0;
+var county = 0;
+
+for (var i = 1; i < xwidth - xlimit; i++) {
+  j = i * xlimit;
   ctx.beginPath();
-  ctx.lineWidth=2;
-  ctx.strokeStyle = strkcol;
-  var step = 20;
-  var newy = yheight/2;
-  var newx = xlimit;
-  for (var o = 0; o<countx; o= o +step){
-    ctx.moveTo(newx, newy);
-    newy = Math.floor(Math.random() * yheight) + 1;
-    newx = newx + step;
-    ctx.lineTo(newx, newy);
-    ctx.stroke();
-  }
+  ctx.lineWidth = 0.5;
+  ctx.strokeStyle = '#1dd623';
+  ctx.moveTo(j, ylimit);
+  ctx.lineTo(j, yheight);
+  ctx.stroke(); // Draw it
   ctx.closePath();
+  countx += 1;
+}
+for (var i = 1; i < yheight - ylimit; i++) {
+  ctx.beginPath();
+  ctx.lineWidth = 0.5;
+  ctx.strokeStyle = '#1dd623';
+  j = i * ylimit;
+  ctx.moveTo(xlimit, j);
+  ctx.lineTo(xwidth, j);
+  ctx.stroke(); // Draw it
+  ctx.closePath();
+
+}
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.strokeStyle = strkcol;
+var step = 20;
+var newy = yheight / 2;
+var newx = xlimit;
+for (var o = 0; o < countx; o = o + step) {
+  ctx.moveTo(newx, newy);
+  newy = Math.floor(Math.random() * yheight) + 1;
+  newx = newx + step;
+  ctx.lineTo(newx, newy);
+  ctx.stroke();
+}
+ctx.closePath();
 
 }
 
@@ -176,7 +189,7 @@ function resizer() {
 function drawcanva() {
   var cnv = document.getElementById('headcanvas');
   var ctx = cnv.getContext('2d');
-  cnv.width = 2000;
+  cnv.width = window.innerWidth;
   cnv.height = 110;
   cnv.style.backgroundColor = "transparent";
   var startx = 0;
@@ -212,9 +225,4 @@ function drawcanva() {
     i = i + 1;
 
   }, 50);
-
-
-
-
-
 }
